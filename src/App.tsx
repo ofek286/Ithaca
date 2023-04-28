1dbc990eb948648f0fa992a971ff3d0e52e1522e
Ofek Segal <ofek286@gmail.com>
2023-04-28 18:39:40 +0300

    Added cards template

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import TinderCard from 'react-tinder-card'
+
+import './App.css'
+import Cards from './Cards'
+
+type Direction = 'left' | 'right' | 'up' | 'down'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [ithacaCards, setIthacaCards] = useState([Cards[0]])
+  const [lastDirection, setLastDirection] = useState<Direction>('right')
+
+  const swiped = (direction: Direction, cardNumber: number) => {
+    if (direction === 'up' || direction === 'down') {
+      return
+    }
+    console.log('removing: ' + cardNumber)
+    var currCard = Cards[cardNumber]
+    if (currCard != null) {
+      var nextCard = currCard[direction]
+      if (nextCard != null) {
+        setIthacaCards([Cards[nextCard]])
+        // ithacaCards.push(Cards[nextCard])
+      }
+    }
+    setLastDirection(direction)
+  }
+
+  const outOfFrame = (cardNumber: number) => {
+    console.log(cardNumber + ' left the screen!')
+  }
+
+  return <div className='app'>
+    <div>
+      <link href='https://fonts.googleapis.com/css?family=Damion&display=swap' rel='stylesheet' />
+      <link href='https://fonts.googleapis.com/css?family=Alatsi&display=swap' rel='stylesheet' />
+      <h1>המסע לאיתקה</h1>
+      <div className='cardRow'>
+        <div className='nextCard'>
+            {ithacaCards[0].left}
+          </div>
+        
+        <div className='cardContainer'>
+          {ithacaCards.map((card) =>
+            <TinderCard
+              className='swipe'
+              key={card.num}
+              onSwipe={(dir) => swiped(dir, card.num)}
+              onCardLeftScreen={() => outOfFrame(card.num)}
+              swipeRequirementType="position"
+              swipeThreshold={100}
+              preventSwipe={
+                card.right == null ? 
+                  (
+                    card.left == null ? ['up', 'down', 'right', 'left'] : ['up', 'down', 'right']
+                  ) :
+                  (
+                    card.left == null ? ['up', 'down', 'left'] : ['up', 'down']
+                  ) 
+              }
+            >
+              <div style={{ backgroundImage: 'url(' + card.img + ')' }} className='card'>
+                <h3>{card.img}</h3>
+              </div>
+            </TinderCard>
+          )}
+        </div>
+        <div className='nextCard'>
+            {ithacaCards[0].right}
+          </div>
+      </div>
+      {/* {
+        lastDirection ?
+          <h2 className='infoText'>You swiped {lastDirection}</h2> : <h2 className='infoText' />
+      } */}
     </div>
-  );
+  </div>
 }
 
-export default App;
+export default App
